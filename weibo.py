039a651541378c957dc32d3c3466e03f544687e7
dongwushenhua <976680003@qq.com>
2022-02-06 00:27:06 +0800

    0:26

--- weibo.py
+++ weibo.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 import json
 import requests
 
-
 headers = {
     'Cookie': 'XSRF-TOKEN=HTjCiwdScQW8CWyc4qSnSSPP; _s_tentry=weibo.com; Apache=8965520555172.314.1644034670529; SINAGLOBAL=8965520555172.314.1644034670529; ULV=1644034670648:1:1:1:8965520555172.314.1644034670529:; login_sid_t=b4dc147db401865d7abab9c3cdb2f47e; cross_origin_proto=SSL; WBStorage=09a9c7be|undefined; wb_view_log=1536*8641.25; SUB=_2A25M-Y_sDeRhGeRI4lQW9i3PyT-IHXVvjuYkrDV8PUNbmtB-LW6kkW9NUpW_xFHV1tJliUHKbsOEe5QntrnnzleP; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5vwXwAn2vQGw17P9apD8CS5JpX5KzhUgL.Fozc1KqNSoe0eoe2dJLoIp7LxKqL1KqLBo5LxK-L122LBK5LxK-L12BLBoUG; ALF=1675572028; SSOLoginState=1644036028; WBPSESS=Dt2hbAUaXfkVprjyrAZT_OyBZ2T6nFRc_MuRwcsUyP-Eg4XHMCZjp_98X05Km-N5iDAy3mFETXW5FpxYW6fPpPqLJJ74-GN5DCINbSVXPzEOFOf1kCtZsf5zB0s-BYf6btV5Ai27FKjMiQ8qhgNdMuhqgSa8C3H2xyh2w8tPws7geZSeJ_OI0KIIZaFRmWdStnClxwCQERuUZD914HbOWw==',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -62,7 +61,7 @@ def deleteAuto():
         # time.sleep(1)
         # url = '//*[@id="scroller"]/div[1]/div[' + i + \
         #       ']/div/article/div[2]/header/div[2]/div[2]/div/div/div[7]'
-        if (len(bro.find_element_by_xpath(url) > 0)):
+        if (-1 > 0):
             f_div = bro.find_element_by_xpath(url)
             f_div.click()
             time.sleep(1)
@@ -70,19 +69,25 @@ def deleteAuto():
                   ']/div/article/div[2]/header/div[2]/div[2]/div/div/div[7]'
             f_div = bro.find_element_by_xpath(url)
             f_div.click()
-        else:
-            url = '//*[@id="scroller"]/div[1]/div['+i + \
-                ']/div/article/div/header/div[2]/div/span/div/i'
-            e_tag = bro.find_element_by_xpath(url)
-            e_tag.click()
-            url = '//*[@id = "scroller"]/div[1]/div[' + i + \
-                ']/div/article/div[2]/header/div[2]/div/div/div/div[1]'
-            e_tag = bro.find_element_by_xpath(url)
-            e_tag.click()
             time.sleep(1)
-        url = '//*[@id="app"]/div[3]/div[1]/div/div[3]/button[2]'
-        g_button = bro.find_element_by_xpath(url)
-        g_button.click()
+        else:
+            url = '//*[@id="scroller"]/div[1]/div[' + i + \
+                  ']/div/article/div/header/div[2]/div/span/div/i'
+            e_tag = bro.find_element_by_xpath(url)
+            if e_tag.get_attribute('title') == "负反馈":
+                continue
+            else:
+                e_tag.click()
+                time.sleep(1)
+                url = '//*[@id = "scroller"]/div[1]/div[' + i + \
+                      ']/div/article/div[2]/header/div[2]/div/div/div/div[1]'
+                e_tag = bro.find_element_by_xpath(url)
+                e_tag.click()
+                time.sleep(1)
+                url = '//*[@id="app"]/div[3]/div[1]/div/div[3]/button[2]'
+                g_button = bro.find_element_by_xpath(url)
+                g_button.click()
+                time.sleep(1)
 
 
 if __name__ == '__main__':
